11a285d7e2f0cdab0c3fad9e490eb16fae87e4fd
Irfan Anshari <irfananshr.id@gmail.com>
2025-01-24 19:39:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper',{
+    effect: 'cube',
+    grabCursor: true,
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 1500,
+        pauseOnMouseEnter: true,
+    },
+});
