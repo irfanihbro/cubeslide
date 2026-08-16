20c6c62d4ffcc91262365c571e5f38982a650b94
Irfan Anshari <irfanxdev@tuta.io>
2026-08-16 23:44:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-new Swiper('.swiper',{
-    effect: 'cube',
-    grabCursor: true,
-    loop: true,
-    speed: 1000,
-    autoplay: {
-        delay: 2000,
-        pauseOnMouseEnter: true,
-    },
+new Swiper(".swiper", {
+  effect: "cube",
+  grabCursor: true,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 1250,
+    pauseOnMouseEnter: true,
+  },
+});
+
+window.addEventListener("load", () => {
+  const audio = document.getElementById("bgMusic");
+
+  audio.currentTime = 0;
+
+  audio.play().catch(() => {
+    console.log("Autoplay diblokir oleh browser. Menunggu interaksi pengguna.");
+
+    const startAudio = () => {
+      audio.currentTime = 0;
+      audio.play();
+
+      document.removeEventListener("click", startAudio);
+      document.removeEventListener("touchstart", startAudio);
+      document.removeEventListener("keydown", startAudio);
+    };
+
+    document.addEventListener("click", startAudio);
+    document.addEventListener("touchstart", startAudio);
+    document.addEventListener("keydown", startAudio);
+  });
 });
